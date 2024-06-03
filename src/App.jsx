899b2168d46566bd7cbf1f287ce87e38f1d5f48d
Escanor360 <abhijithca360@gmail.com>
2024-06-03 07:45:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Navigate to="/login" />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
